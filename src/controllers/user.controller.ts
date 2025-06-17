c3c80f9e7b00c9bd6b3968724da5114a53375b22
Andss-ye <andrex120tv@gmail.com>
2025-06-16 19:18:51 -0500

    refactor: :recycle: actualizacion de logica de la creacion del usuario

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,23 +1,20 @@
 import { Request, Response, NextFunction } from "express";
-import { createUserService } from "../services/user.service";
+import * as userService from "../services/user.service";
 
 //importing middlewares
 import createError from "http-errors";
 
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
     const { name, email } = req.body;
 
     if (!name || !email) {
-      return next(createError(400, 'Email and name are required'));
+        return next(createError(400, 'Email and name are required'));
     }
 
-    const user = await createUserService({ name, email });
-    res.status(201).json({ user });
-  } catch (error: any) {
-    if (error.code === "P2002") {
-      return next(createError(409, 'Email already exists'))
+    if (await userService.getUserByEmail(email)) {
+        return next(createError(409, 'Email already exists'))
     }
-    return next(createError(500, 'Error creating user'))
-  }
+
+    const user = await userService.createUser({ name, email });
+    res.status(201).json({ user });
 };
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,11 +2,20 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const createUserService = async (userData: { name: string; email: string }) => {
+export const createUser = async (userData: { name: string; email: string }) => {
     const user = await prisma.user.create({
         data: {
             name: userData.name,
             email: userData.email
+        }
+    });
+    return user;
+};
+
+export const getUserByEmail = async (email: string) => {
+    const user = await prisma.user.findFirst({
+        where: {
+            email
         }
     });
     return user;
