2759b126b25112244c38df9769b1e4bb1b629819
Andss-ye <andrex120tv@gmail.com>
2025-06-17 21:13:31 -0500

    feat: :sparkles: pequeno cambio hecho en el swagger para que admita la url de heroku

--- src/swagger.ts
+++ src/swagger.ts
@@ -9,7 +9,7 @@ export const swaggerSpec = swaggerJSDoc({
     },
     servers: [
       {
-        url: "http://localhost:4000/api",
+        url: process.env.BASE_URL || "http://localhost:4000/",
       }
     ],
     tags: [
