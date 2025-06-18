e57137f7805866622607031045b8bf403d45aa38
Andss-ye <andrex120tv@gmail.com>
2025-06-17 21:05:08 -0500

    feat: :sparkles: Se agrego el cors

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { swaggerSpec } from "./swagger";
 
 // Importing middlewares
 import errorHandler from './middlewares/errorHandler';
+import cors from "cors"
 
 // Importing routes
 import userRoutes from './routes/user.routes';
@@ -14,8 +15,13 @@ dotenv.config();
 
 const app = express();
 
-
 app.use(express.json());
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
 
 app.get('/', (_req, res) => {
     res.status(200).json({ message: "Hola movieAPI 🚀" });
